5f17f4218cdb577f03976775082cb8f69da2137e
wujiake <347129540@qq.com>
2020-08-14 09:55:33 +0800

    新增文件读写

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+//@SpringBootTest
 class DemoApplicationTests {
 
 
@@ -20,6 +23,23 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void readAndOutFiles() throws IOException {
+        File input = new File("E:\\chromeDownload\\hbaseout1.txt");
+        File out = new File("C:\\Users\\her\\Desktop\\out.txt");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(input), StandardCharsets.UTF_8));
+        FileOutputStream fileOutputStream = new FileOutputStream(out);
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            String[] s = line.split(" ");
+            String outString = "deleteall \'freeflow_dev:tb_all_duplicate_check\', \'" + line.split(" ")[1] + "\'\n";
+            fileOutputStream.write(outString.getBytes());
+        }
+        fileOutputStream.close();
+        br.close();
     }
 
 }
